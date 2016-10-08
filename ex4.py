b25b6dc1ace9c5d6cfd308734730df88cebe4428
Arun Khanna <arunkhanna00@gmail.com>
2016-10-08 10:18:26 -0700

    complete ex4.py

--- ex4.py
+++ ex4.py
@@ -1,20 +1,29 @@
-print "I will now count my chickens:"
+# There are 100 cars
+cars = 100
+# There are 4 spaces in the car
+space_in_a_car = 4.0
+# There are 30 drivers
+drivers = 30
+# There are 90 pasengers
+passengers = 90
+# The cars not driven are the cars subtracted by the number of drivers
+cars_not_driven = cars - drivers
+# The cars driven are equal to the number of drivers
+cars_driven = drivers
+# The capacity of the cars is the number of cars being driven multiplied by the number of spaces in each car
+carpool_capacity = cars_driven * space_in_a_car
+# The average passenger per car is the number of passengers divided by the number of cars
+average_passengers_per_car = passengers / cars_driven
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
-
-print "Now I will count the eggs:"
-
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
-print "Is it true that 3 + 2 < 5 - 7?"
-
-print 3 + 2 < 5 - 7
-
-print "What is 3 + 2?", 3 + 2
-print "Oh, that's why it's False."
-
-print "How about some more."
-
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 <= -2
+# Print the number of cars available
+print "There are", cars, "cars available."
+# Print the number of drivers available
+print "There are only", drivers, "drivers available."
+# Print the number of empty cars
+print "There will be", cars_not_driven, "empty cars today."
+# Print the capacity of all the cars that can be driven
+print "We can transport", carpool_capacity, "people today."
+# Print the number of passengers
+print "We have", passengers, "to carpool today."
+# Print the avergage passenger per car
+print "We need to put about", average_passengers_per_car, "in each car."
